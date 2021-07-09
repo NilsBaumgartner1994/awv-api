a2ec20f2404b70763d21b13dbaa6667e17f3d513
Nils <nilsbaumgartner1994@gmail.com>
2021-07-09 16:50:16 +0200

    Changed Testing file

--- src/tests/test.ts
+++ src/tests/test.ts
@@ -3,5 +3,6 @@ import FetchHelper from '../FetchHelper';
 test('Server start Test', (): void => {
   const options = {};
   const server = new FetchHelper();
+  console.log('Hello Testing');
   expect('Starting').toBe('Starting');
 });
