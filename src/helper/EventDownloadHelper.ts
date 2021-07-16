92ef170b6618277fbccc5f268b29e72a9862ee46
Lint Action <lint-action@samuelmeuli.com>
2021-07-16 22:17:51 +0000

    Fix code style issues with Prettier

--- src/helper/EventDownloadHelper.ts
+++ src/helper/EventDownloadHelper.ts
@@ -2,12 +2,12 @@
 
 import UrlHelper from './UrlHelper';
 import FetchHelper from '../ignoreCoverage/FetchHelper';
-import City from "../models/City";
-import Event from "../models/Event";
-import Street from "../models/Street";
+import City from '../models/City';
+import Event from '../models/Event';
+import Street from '../models/Street';
 import {parse} from 'node-html-parser';
-import {Provider} from "../models/Provider";
-import EventFilterHelper from "./EventFilterHelper";
+import {Provider} from '../models/Provider';
+import EventFilterHelper from './EventFilterHelper';
 
 /**
  * EventDownloadHelper class
@@ -15,76 +15,141 @@ import EventFilterHelper from "./EventFilterHelper";
  * @class EventDownloadHelper
  */
 export default class EventDownloadHelper {
-
-  static async downloadEventsAndAddToCities(year: number, cities: City[]) : Promise<City[]> {
+  static async downloadEventsAndAddToCities(
+    year: number,
+    cities: City[]
+  ): Promise<City[]> {
     let citiesWithEvents: City[] = [];
-    for(const city of cities){
-      const cityWithEvents: City = await EventDownloadHelper.downloadEventsAndAddToCity(year, city);
+    for (const city of cities) {
+      const cityWithEvents: City =
+        await EventDownloadHelper.downloadEventsAndAddToCity(year, city);
       citiesWithEvents.push(cityWithEvents);
     }
     return citiesWithEvents;
   }
 
-  static async downloadEventsAndAddToCity(year: number, city: City) : Promise<City>{
+  static async downloadEventsAndAddToCity(
+    year: number,
+    city: City
+  ): Promise<City> {
     let streetsWithEvents: Street[] = [];
     const streets = city.getStreets();
-    for(const street of streets){
-      const streetWithEvents = await EventDownloadHelper.downloadEventsAndAddToStreet(year, city, street);
+    for (const street of streets) {
+      const streetWithEvents =
+        await EventDownloadHelper.downloadEventsAndAddToStreet(
+          year,
+          city,
+          street
+        );
       streetsWithEvents.push(streetWithEvents);
     }
     city.setStreets(streetsWithEvents);
     return city;
   }
 
-  static async downloadEventsAndAddToStreet(year: number, city: City, street: Street) : Promise<Street> {
-    let events: Event[] = await EventDownloadHelper.downloadEventsForStreet(year, city, street);
+  static async downloadEventsAndAddToStreet(
+    year: number,
+    city: City,
+    street: Street
+  ): Promise<Street> {
+    let events: Event[] = await EventDownloadHelper.downloadEventsForStreet(
+      year,
+      city,
+      street
+    );
     street.setEvents(events);
     return street;
   }
 
-  static async downloadEventsForStreet(year: number, city: City, street: Street) : Promise<Event[]>{
-    let vechtaEvents: Event[] = await EventDownloadHelper.downloadEvents(year, city, street, Provider.VECHTA, []);
-    let rawPamoEvents: Event[] = await EventDownloadHelper.downloadEvents(year, city, street, Provider.PAMO, vechtaEvents);
-    let rawSiemerEvents: Event[] = await EventDownloadHelper.downloadEvents(year, city, street, Provider.SIEMER, vechtaEvents);
+  static async downloadEventsForStreet(
+    year: number,
+    city: City,
+    street: Street
+  ): Promise<Event[]> {
+    let vechtaEvents: Event[] = await EventDownloadHelper.downloadEvents(
+      year,
+      city,
+      street,
+      Provider.VECHTA,
+      []
+    );
+    let rawPamoEvents: Event[] = await EventDownloadHelper.downloadEvents(
+      year,
+      city,
+      street,
+      Provider.PAMO,
+      vechtaEvents
+    );
+    let rawSiemerEvents: Event[] = await EventDownloadHelper.downloadEvents(
+      year,
+      city,
+      street,
+      Provider.SIEMER,
+      vechtaEvents
+    );
     let mergedEvents = vechtaEvents.concat(rawPamoEvents, rawSiemerEvents);
     return EventFilterHelper.sortListOfEvents(mergedEvents);
   }
 
-  private static async downloadEvents(year: number, city: City, street: Street, provider: Provider, baseEvents: Event[]) : Promise<Event[]> {
+  private static async downloadEvents(
+    year: number,
+    city: City,
+    street: Street,
+    provider: Provider,
+    baseEvents: Event[]
+  ): Promise<Event[]> {
     const providerField = UrlHelper.getProviderSearchField(provider);
     const abfuhrbezirkpapier = street.getProviderValue(provider);
     const url = UrlHelper.CALENDAR_URL;
 
     let events: Event[] = [];
 
-    if(abfuhrbezirkpapier !== "0"){
-      let response = await FetchHelper.fetchWithCookie(url, year, city.id, street.id, street.abfuhrbezirk, providerField, abfuhrbezirkpapier);
+    if (abfuhrbezirkpapier !== '0') {
+      let response = await FetchHelper.fetchWithCookie(
+        url,
+        year,
+        city.id,
+        street.id,
+        street.abfuhrbezirk,
+        providerField,
+        abfuhrbezirkpapier
+      );
       let html = await response.text();
-      events = EventDownloadHelper.parseWebsiteTextToEvents(html, provider, year, baseEvents);
+      events = EventDownloadHelper.parseWebsiteTextToEvents(
+        html,
+        provider,
+        year,
+        baseEvents
+      );
     }
 
     return events;
   }
 
-  private static parseWebsiteTextToEvents(html: string, provider: Provider, year: number, baseEvents: Event[]): Event[]{
+  private static parseWebsiteTextToEvents(
+    html: string,
+    provider: Provider,
+    year: number,
+    baseEvents: Event[]
+  ): Event[] {
     let events: Event[] = [];
-    let dictOfBaseEvents = EventFilterHelper.parseListOfEventsToDict(baseEvents);
+    let dictOfBaseEvents =
+      EventFilterHelper.parseListOfEventsToDict(baseEvents);
 
     const root = parse(html);
-    let elements = root.querySelectorAll("span");
+    let elements = root.querySelectorAll('span');
     for (let element of elements) {
-      if (element.classList.contains("month_termin")) {
+      if (element.classList.contains('month_termin')) {
         let text = element.text; // 05.01 : Restabfall
-        let split = text.split(" : ");
-        let dateAsString = split[0] + "." + year;
+        let split = text.split(' : ');
+        let dateAsString = split[0] + '.' + year;
         let label = split[1];
         let event: Event = new Event(dateAsString, label, provider);
-        if(!EventFilterHelper.isEventInDictOfEvents(event, dictOfBaseEvents)){
+        if (!EventFilterHelper.isEventInDictOfEvents(event, dictOfBaseEvents)) {
           events.push(event);
         }
       }
     }
     return events;
   }
-
 }
--- src/helper/EventFilterHelper.ts
+++ src/helper/EventFilterHelper.ts
@@ -1,34 +1,38 @@
-import Event from "../models/Event";
+import Event from '../models/Event';
 
-export default class EventFilterHelper{
+export default class EventFilterHelper {
+  static eventToComparableString(event: Event): string {
+    return event.date + '$' + event.label; //the informations from the website are only date and label
+    //so we cant use anything else to use for a comparable string
+  }
 
-    static eventToComparableString(event: Event) : string{
-        return event.date+"$"+event.label; //the informations from the website are only date and label
-        //so we cant use anything else to use for a comparable string
+  static isEventInDictOfEvents(
+    event: Event,
+    dictOfEvents: {[key: string]: Event}
+  ): boolean {
+    const key: string = EventFilterHelper.eventToComparableString(event);
+    return !!dictOfEvents[key];
+  }
+
+  static parseListOfEventsToDict(listOfEvents: Event[]): {
+    [key: string]: Event;
+  } {
+    let dictDateLabelStrings: {[key: string]: Event} = {};
+    for (let event of listOfEvents) {
+      const key: string = EventFilterHelper.eventToComparableString(event);
+      dictDateLabelStrings[key] = event;
     }
+    return dictDateLabelStrings;
+  }
 
-    static isEventInDictOfEvents(event : Event, dictOfEvents: { [key: string] : Event}) : boolean {
-        const key: string = EventFilterHelper.eventToComparableString(event);
-        return !!dictOfEvents[key];
-    }
+  static sortListOfEvents(listOfEvents: Event[]) {
+    listOfEvents.sort(EventFilterHelper.compareEvent);
+    return listOfEvents;
+  }
 
-    static parseListOfEventsToDict(listOfEvents : Event[]) : { [key: string] : Event} {
-        let dictDateLabelStrings: { [key: string] : Event; } = {};
-        for(let event of listOfEvents){
-            const key : string = EventFilterHelper.eventToComparableString(event);
-            dictDateLabelStrings[key] = event;
-        }
-        return dictDateLabelStrings;
-    }
-
-    static sortListOfEvents(listOfEvents: Event[]){
-        listOfEvents.sort(EventFilterHelper.compareEvent);
-        return listOfEvents;
-    }
-
-    static compareEvent(a : Event, b : Event){
-        let dateOfA = a.getDate();
-        let dateOfB = b.getDate();
-        return dateOfA.getTime()-dateOfB.getTime();
-    }
+  static compareEvent(a: Event, b: Event) {
+    let dateOfA = a.getDate();
+    let dateOfB = b.getDate();
+    return dateOfA.getTime() - dateOfB.getTime();
+  }
 }
