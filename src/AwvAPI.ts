8a83afb83668547bbad0cbe69b9e21bc2f4044b4
Nils <nilsbaumgartner1994@gmail.com>
2021-07-17 00:44:00 +0200

    Revert "Added CompoDoc test"
    This reverts commit 85e995f5326791d0507e110b4c882fce00d627b1.

--- src/AwvAPI.ts
+++ src/AwvAPI.ts
@@ -6,13 +6,12 @@ import Street from './models/Street';
 
 /**
  * AwvAPI class
- * this is a comment
  *
  * @class AwvAPI
  */
 export default class AwvAPI {
   static async downloadAllCitiesAndStreets(year: number) {
-    const allCities = await AwvAPI.downloadAllCities(year); // get all cities
+    const allCities = await AwvAPI.downloadAllCities(year);
     for (const city of allCities) {
       const allStreetsForCity = await AwvAPI.downloadAllStreetsForCity(
         year,
