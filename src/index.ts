cd6774f2549834c7b1d6f69e82676d0b7dbce9e5
Nils <nilsbaumgartner1994@gmail.com>
2021-08-29 11:50:06 +0200

    Added Export

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,12 @@ import Street from './models/Street';
 import Event from './models/Event';
 import {Provider} from './models/Provider';
 import EventHelper from './helper/EventHelper';
+import { EventLabels } from './models/EventLabels';
 
 export {AwvAPI};
 export {City};
 export {Street};
 export {Event};
+export {EventLabels};
 export {Provider};
 export {EventHelper};
